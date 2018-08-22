f576282b1c14d97c8d7629b328e8b6fe2f430f9c
Y_林泉 <32585968+KamyoChae@users.noreply.github.com>
2018-08-22 13:15:44 +0800

    Create index.js

---
+++ magnifier/js/index.js
@@ -0,0 +1,5 @@
+magnifier({
+    show : document.getElementsByClassName("show")[0], // 用于展示放大图片的元素
+    box : document.getElementsByClassName("box")[0], // 用于正常状态图片的元素
+    rect : document.getElementsByClassName("rect")[0] // 获取半透明色块
+})
